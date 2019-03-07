d86558f1ed87c5c35f3a03fb59ed7eb1cde25837
carlosmalaquias <carlosomalaquias@gmail.com>
2019-03-07 02:58:19 -0300

    First Commit

---
+++ src/control/ClienteControl.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.security.Provider;
+import java.util.ArrayList;
+import java.util.List;
+import model.dao.ClienteDao;
+import model.dao.ClienteDaoImpl;
+import model.domain.Cliente;
+import org.jdesktop.observablecollections.ObservableCollections;
+import util.ValidacaoException;
+
+/**
+ *
+ * @author malaquias
+ */
+public final class ClienteControl {
+    
+    private PropertyChangeSupport propertyChangeSupport =
+            new PropertyChangeSupport(this);
+    
+    private Cliente clienteDigitado;
+    
+    private Cliente clienteSelecionado;
+    
+    private List<Cliente> clientesTabela;
+    
+    private ClienteDao clienteDao;
+    
+    public ClienteControl() {
+         clienteDao = ServiceLocator.getClienteDao();
+         clientesTabela = ObservableCollections.observableList(new ArrayList<Cliente>());
+         novo();
+         pesquisar();
+    }
+
+    public void novo() {
+        setClienteDigitado(new Cliente());
+    }
+
+    public void pesquisar() {
+        clientesTabela.clear();
+        clientesTabela.addAll(clienteDao.pesquisar(clienteDigitado));
+    }
+    
+    public void salvar() throws ValidacaoException {
+        clienteDigitado.validar();
+        clienteDao.salvarAtualizar(clienteDigitado);
+        novo();
+        pesquisar();
+    }
+    
+    public void excluir() {
+        clienteDao.excluir(clienteDigitado);
+        novo();
+        pesquisar();
+    }
+
+    public Cliente getClienteDigitado() {
+        return clienteDigitado;
+    }
+
+    public void setClienteDigitado(Cliente clienteDigitado) {
+        Cliente oldClienteDigitado = this.clienteDigitado;
+        this.clienteDigitado = clienteDigitado;
+        propertyChangeSupport.firePropertyChange(
+        "clienteDigitado", oldClienteDigitado, clienteDigitado);
+    }
+
+    public Cliente getClienteSelecionado() {
+        return clienteSelecionado;
+    }
+
+    public void setClienteSelecionado(Cliente clienteSelecionado) {
+        this.clienteSelecionado = clienteSelecionado;
+        if(this.clienteSelecionado != null) {
+            setClienteDigitado(clienteSelecionado);
+        }
+    }
+
+    public List<Cliente> getClientesTabela() {
+        return clientesTabela;
+    }
+
+    public void setClientesTabela(List<Cliente> clientesTabela) {
+        System.out.println("ccccccccccc.......");
+        this.clientesTabela = clientesTabela;
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
